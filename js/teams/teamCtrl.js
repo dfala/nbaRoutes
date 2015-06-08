1ef0676b476ec21b1a24ca38e29878929dd8ed6d
Daniel Falabella <dnlfala@gmail.com>
2015-06-08 16:33:34 -0600

    Finished homework

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,45 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	$scope.newGame;
+	$scope.showNewGameForm = false;
 
+	if (teamData[0].homeTeam === 'utahjazz') {
+		$scope.homeTeam = 'Utah Jazz';
+		$scope.logoPath = 'images/jazz-logo.png';
+	} else if (teamData[0].homeTeam === 'losangeleslakers') {
+		$scope.homeTeam = 'Los Angeles Lakers';
+		$scope.logoPath = 'images/lakers-logo.png';
+	} else if (teamData[0].homeTeam === 'miamiheat') {
+		$scope.homeTeam = 'Miami Heat';
+		$scope.logoPath = 'images/heat-logo.png';
+	}
+
+	$scope.toggleNewGameForm = function () {
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
+
+	$scope.submitGame = function () {
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+
+		console.log($scope.newGame);
+
+		teamService.addNewGame($scope.newGame)
+			.then(function (response) {
+				console.log(response, 'data data data data!!!');
+				teamService.getTeamData($scope.newGame.homeTeam)
+					.then(function (data) {
+						// console.log(data, 'data data data data!!!');
+						$scope.teamData = data;
+
+						$scope.newGame = '';
+						$scope.showNewGameForm = false;
+					})
+			}, function (err) {
+				console.error(err);
+			})
+	}
+
+	// console.warn('$scope', $scope);
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,62 @@
 var app = angular.module('nbaRoutes');
 
-app.service('teamService', function($http, $q){
+app.factory('teamService', function($http, $q){
+	var service = {};
 
+	// ADD NEW GAME
+	service.addNewGame = function (gameObject) {
+		var deferred = $q.defer();
+
+		if (!gameObject) return console.warn('gameObject is not defined');
+
+		//gameObject will have the data bout individual game that we'll send to parse
+		var url = 'https://api.parse.com/1/classes/' + gameObject.homeTeam;
+		
+		var homeTeamScore = parseInt(gameObject.homeTeamScore);
+		var opponentScore = parseInt(gameObject.opponentScore);
+
+
+		if (homeTeamScore > opponentScore) {
+			gameObject.win = false;
+		};
+
+		$http.post(url, gameObject)
+			.then(function (response) {
+				deferred.resolve(response);
+			}, function (err) {
+				console.error(err)
+			})
+
+		return deferred.promise;
+	}
+
+	// ADD NEW GAME
+	service.getTeamData = function (team) {
+		if (!team) return console.warn('team is not defined');
+
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team + '?order=-createdAt';
+
+		$http.get(url)
+			.then(function (response) {
+				var results = response.data.results;
+				var wins = 0;
+				var losses = 0;
+
+				results.forEach(function (game, index) {
+					if (game.won) { wins++ } else { losses++ };
+				})
+
+				results.wins = wins;
+				results.losses = losses;
+
+				deferred.resolve(results);
+			}, function (err) {
+				console.error(err);
+			})
+
+		return deferred.promise;
+	}
+
+	return service;
 });
